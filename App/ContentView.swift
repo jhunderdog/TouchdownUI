decd9d3918f32161476c27eeb756f951dfee73e5
jhunderdog <33382water@gmail.com>
2022-02-01 09:52:47 +0900

    lazyHgrid

--- App/ContentView.swift
+++ App/ContentView.swift
@@ -37,6 +37,8 @@ struct ContentView: View {
                             }
                         })
                             .padding(15)
+                        TitleView(title: "Brands")
+                        BrandGridView()
                         FooterView()
                             .padding(.horizontal)
                     }
@@ -55,5 +57,6 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
             .previewDevice("iPhone 12 Pro")
+.previewInterfaceOrientation(.landscapeLeft)
     }
 }
---
+++ Model/BrandModel.swift
@@ -0,0 +1,13 @@
+//
+//  BrandModel.swift
+//  Touchdown
+//
+//  Created by jhunderdog on 2022/02/01.
+//
+
+import Foundation
+
+struct Brand: Codable, Identifiable {
+    let id: Int
+    let image: String
+}
